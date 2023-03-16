380db41c19a69e99e6b2e7e53c4a96ef3e8f61fe
Sebastian R Loguzzo <Sebastian.loguzzo@gmail.com>
2023-03-15 23:35:50 -0300

    feat: Add loader to images to improve UX

--- src/components/RowTable.js
+++ src/components/RowTable.js
@@ -1,35 +1,52 @@
-import { useNavigation } from "@react-navigation/native";
-import React from "react";
-import { View, Text, Image, TouchableOpacity } from "react-native";
+import { useNavigation } from '@react-navigation/native';
+import React, { useState } from 'react';
+import {
+  View,
+  Text,
+  Image,
+  TouchableOpacity,
+  ActivityIndicator,
+} from 'react-native';
 
-import { styles } from "../styles";
-import { formatDate } from "../utils";
+import { styles } from '../styles';
+import { formatDate } from '../utils';
 
 const RowTable = ({ product, points, isRedemption, date, image }) => {
   const navigation = useNavigation();
+  const [loading, setLoading] = useState(true);
 
   return (
     <TouchableOpacity
       testID="touchableProduct"
       onPress={() => {
-        navigation.navigate("Product", {
+        navigation.navigate('Product', {
           product,
           points,
           isRedemption,
           date,
           image,
         });
-      }}
-    >
+      }}>
       <View style={styles.rowTable}>
+        {loading && (
+          <View
+            style={{
+              width: 55,
+              height: 55,
+              position: 'absolute',
+              justifyContent: 'center',
+            }}>
+            <ActivityIndicator color="#334FFA" />
+          </View>
+        )}
         <Image
           source={{ uri: image }}
           style={{ width: 55, height: 55, borderRadius: 10 }}
+          onLoadEnd={() => setLoading(false)}
         />
         <View style={{ marginLeft: 12, width: 200 }}>
           <Text
-            style={[styles.titleSection, { color: "#000", marginVertical: 5 }]}
-          >
+            style={[styles.titleSection, { color: '#000', marginVertical: 5 }]}>
             {product}
           </Text>
           <Text style={[styles.dateText, { marginTop: 2 }]}>
@@ -38,20 +55,18 @@ const RowTable = ({ product, points, isRedemption, date, image }) => {
         </View>
         <View
           style={{
-            alignItems: "center",
-            flexDirection: "row",
-          }}
-        >
+            alignItems: 'center',
+            flexDirection: 'row',
+          }}>
           {isRedemption ? (
-            <Text style={[styles.monthBlueBox, { color: "#FF0000" }]}>- </Text>
+            <Text style={[styles.monthBlueBox, { color: '#FF0000' }]}>- </Text>
           ) : (
-            <Text style={[styles.monthBlueBox, { color: "#00B833" }]}>+ </Text>
+            <Text style={[styles.monthBlueBox, { color: '#00B833' }]}>+ </Text>
           )}
-          <Text style={[styles.monthBlueBox, { color: "#000" }]}>{points}</Text>
-          <View style={{ justifyContent: "center", alignItems: "flex-end" }}>
+          <Text style={[styles.monthBlueBox, { color: '#000' }]}>{points}</Text>
+          <View style={{ justifyContent: 'center', alignItems: 'flex-end' }}>
             <Text
-              style={[styles.monthBlueBox, { color: "#000", marginLeft: 8 }]}
-            >
+              style={[styles.monthBlueBox, { color: '#000', marginLeft: 8 }]}>
               &gt;
             </Text>
           </View>
--- src/screens/ProductDetail.js
+++ src/screens/ProductDetail.js
@@ -1,26 +1,45 @@
-import { useNavigation, useRoute } from "@react-navigation/native";
-import React, { useContext } from "react";
-import { View, ScrollView, Image, Text, TouchableOpacity } from "react-native";
-import mainContext from "../../context/mainContext";
-import { CardDetail } from "../components";
+import { useNavigation, useRoute } from '@react-navigation/native';
+import React, { useContext, useState } from 'react';
+import {
+  View,
+  ScrollView,
+  Image,
+  Text,
+  TouchableOpacity,
+  ActivityIndicator,
+} from 'react-native';
+import mainContext from '../../context/mainContext';
+import { CardDetail } from '../components';
 
 function ProductDetail() {
   const { styles } = useContext(mainContext);
   const route = useRoute();
   const { product, points, isRedemption, date, image } = route.params;
   const navigation = useNavigation();
+  const [loading, setLoading] = useState(true);
 
   return (
     <ScrollView
       contentContainerStyle={{
         padding: 20,
-        width: "100%",
-      }}
-    >
+        width: '100%',
+      }}>
       <View style={styles.containerImg}>
+        {loading && (
+          <View
+            style={{
+              width: '100%',
+              height: 350,
+              position: 'absolute',
+              justifyContent: 'center',
+            }}>
+            <ActivityIndicator color="#334FFA" size="large" />
+          </View>
+        )}
         <Image
           source={{ uri: image }}
-          style={{ width: "100%", height: 350, borderRadius: 10 }}
+          style={{ width: '100%', height: 350, borderRadius: 10 }}
+          onLoadEnd={() => setLoading(false)}
         />
       </View>
       <CardDetail
@@ -34,8 +53,7 @@ function ProductDetail() {
         testID="touchableGoBackTolist"
         onPress={() => {
           navigation.goBack();
-        }}
-      >
+        }}>
         <Text style={styles.monthBlueBox}>Aceptar</Text>
       </TouchableOpacity>
     </ScrollView>
